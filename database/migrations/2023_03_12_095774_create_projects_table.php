2bc54e8f4751ae9c0796004c1e6fcc3ee49e4cae
muxalik <muxail1327@mail.ru>
2023-03-12 14:51:24 +0300

    feature(migrations) created migrations

---
+++ database/migrations/2023_03_12_095774_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('team_id')->nullable()->constrained()->nullOnDelete();
+            $table->integer('total_tasks')->unsigned()->default(0);
+            $table->integer('tasks_done')->unsigned()->default(0);
+            $table->integer('total_hours')->unsigned()->default(0);
+            $table->integer('wasted_hours')->unsigned()->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ database/migrations/2023_03_12_095787_create_project_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_user', function (Blueprint $table) {
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->boolean('is_favourite')->default(false);
+            $table->integer('total_tasks')->unsigned()->default(0);
+            $table->integer('tasks_done')->unsigned()->default(0);
+            $table->integer('total_hours')->unsigned()->default(0);
+            $table->integer('wasted_hours')->unsigned()->default(0);
+            $table->timestamps();
+
+            $table->primary(['project_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_user');
+    }
+};

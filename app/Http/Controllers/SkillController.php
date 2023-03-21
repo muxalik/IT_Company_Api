262566273d87dda6f46574d29a20a89cdab286e3
muxalik <muxail1327@mail.ru>
2023-03-21 17:05:03 +0300

    feature(app) add skill controller, requests, resource, collection

---
+++ app/Http/Controllers/SkillController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Skill\StoreRequest;
+use App\Http\Resources\SkillCollection;
+use App\Http\Resources\SkillResource;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(): SkillCollection
+    {
+        return new SkillCollection(Skill::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRequest $request): SkillResource
+    {
+        return $this->skillResponse(Skill::create($request->validated()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Skill  $skill
+     * @return SkillResource
+     */
+    public function show(Skill $skill): SkillResource
+    {
+        return $this->skillResponse($skill);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Skill  $skill
+     * @return SkillResource
+     */
+    public function update(Request $request, Skill $skill): SkillResource
+    {
+        $skill->update($request->validated());
+
+        return $this->skillResponse($skill);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Skill  $skill
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Skill $skill): Response
+    {
+        $skill->delete();
+
+        return response()->noContent();
+    }
+
+    public function skillResponse(Skill $skill): SkillResource
+    {
+        return new SkillResource($skill);
+    }
+}
